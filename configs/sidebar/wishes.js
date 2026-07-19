d8117f1fc9ac08055d7ef5adf6d5d08f2cf9f161
xt <xt@thebestxt.cc>
2026-07-19 21:58:09 +0800

    德墨忒尔祝福

--- configs/sidebar/wishes.js
+++ configs/sidebar/wishes.js
@@ -5,4 +5,5 @@ export default [
     { label: '宙斯', slug: 'wishes/zeus', attrs: { 'data-icon': `${hadesSourceHost}/awardsIcon/Zeus_reward.webp` } },
     { label: '赫拉', slug: 'wishes/hera', attrs: { 'data-icon': `${hadesSourceHost}/awardsIcon/Hera_reward.webp` } },
     { label: '波塞冬', slug: 'wishes/poseidon', attrs: { 'data-icon': `${hadesSourceHost}/awardsIcon/Poseidon_reward.webp` } },
+    { label: '德墨忒尔', slug: 'wishes/demeter', attrs: { 'data-icon': `${hadesSourceHost}/awardsIcon/Demeter_reward.webp` } },
 ]
